c1a89bff1e225531ac4512e168308728464b6d0f
Evan Day <yichaoday_evan@berkeley.edu>
2023-01-01 14:16:27 -0800

    reherse fold-fulkerson

--- CS-170-Lec-14-Networkflow-Ford-Fulkerson.py
+++ CS-170-Lec-14-Networkflow-Ford-Fulkerson.py
@@ -2,45 +2,46 @@
 
 # input in adj matrix representation
 # u[i][j] is capacity of edge (i,j) (0 if edge not present)
-def ford_fulkerson(u, s, t):
-    n = len(u)
-    f = [[0] * n for _ in range(n)]
-    visited = []
 
-    def push_flow(v, amt):
-        nonlocal n, u, t, f, visited
-        if v == t:
+
+def ford_fulkerson(m, s, t):
+    n = len(m)
+    # used f[i][j]
+    f = [[0] * n for _ in range(n)]
+
+    def dfs(u, amt):
+        nonlocal t, visited, f, m, n
+        if u == t:
             return amt
-        for w in range(n):
-            if not visited[w]:
-                residual = u[v][w] - f[v][w]
-                if residual > 0:
-                    visited[w] = True
-                    amt = min(amt, push_flow(w, min(amt, residual)))
-                    f[v][w] += amt
-                    f[w][v] -= amt
+        for v in range(n):
+            if not visited[v]:
+                pushing_ava = m[u][v] - f[u][v]
+                if pushing_ava > 0:
+                    visited[v] = True
+                    amt = min(amt, dfs(v, min(amt, pushing_ava)))
+                    f[u][v] += amt
+                    f[v][u] -= amt
                     return amt
         return 0
 
     while True:
         visited = [False] * n
         visited[s] = True
-        if push_flow(s, float('infinity')) == 0:
+        if dfs(s, float("inf")) == 0:
             break
-
     for i in range(n):
         for j in range(n):
             f[i][j] = max(f[i][j], 0)
-    return f
+    return sum(f[0])
 
 
 capacities = [
-        [0, 1, 1, 0, 0, 0],
-        [0, 0, 0, 1, 1, 0],
-        [0, 0, 0, 1, 0, 0],
-        [0, 0, 0, 0, 0, 1],
-        [0, 0, 0, 0, 0, 1],
-        [0, 0, 0, 0, 0, 0]
-    ]
+    [0, 1, 1, 0, 0, 0],
+    [0, 0, 0, 1, 1, 0],
+    [0, 0, 0, 1, 0, 0],
+    [0, 0, 0, 0, 0, 1],
+    [0, 0, 0, 0, 0, 1],
+    [0, 0, 0, 0, 0, 0]
+]
 
 print(ford_fulkerson(capacities, 0, 5))
